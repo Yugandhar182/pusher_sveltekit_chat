87a0cfb147e183967c17dd98ba9328268325fec8
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-10-02 18:06:50 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.post('/api/messages', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
 });
